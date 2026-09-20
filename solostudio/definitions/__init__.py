c6390810e209527b540a15181199a7b0955c7a3e
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-20 21:51:48 +0300

    M0-020/021: add immutable preset identities

---
+++ solostudio/definitions/__init__.py
@@ -0,0 +1,11 @@
+from .presets import (
+    caption_style_identity,
+    visual_style_identity,
+    voice_profile_identity,
+)
+
+__all__ = [
+    "caption_style_identity",
+    "visual_style_identity",
+    "voice_profile_identity",
+]
