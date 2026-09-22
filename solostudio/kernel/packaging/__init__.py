16f20a9b42e345298aa632b6ac362cfa8fb7b7dd
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-22 08:35:58 +0300

    M0-B/01: expose final Slice 9 package authority

--- solostudio/kernel/packaging/__init__.py
+++ solostudio/kernel/packaging/__init__.py
@@ -1,3 +1,4 @@
-from solostudio.kernel.packaging.service import ContractRevalidation, PackagingService
+from solostudio.kernel.packaging.final import Slice9PackagingService as PackagingService
+from solostudio.kernel.packaging.service import ContractRevalidation
 
 __all__ = ["ContractRevalidation", "PackagingService"]
