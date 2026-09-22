85683c8d8d2aaaf19fe51f769c91dd3dbac42d09
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-09-22 08:42:18 +0300

    M0-B/01: expose linearized destination authority

--- solostudio/kernel/destinations/__init__.py
+++ solostudio/kernel/destinations/__init__.py
@@ -1,3 +1,3 @@
-from solostudio.kernel.destinations.service import DestinationContractService
+from solostudio.kernel.destinations.final import Slice9DestinationContractService as DestinationContractService
 
 __all__ = ["DestinationContractService"]
